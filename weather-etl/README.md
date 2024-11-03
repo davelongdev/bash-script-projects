a03ecad89e4b80560b6bef29065237eb0baedcb1
dave long <davelongdev@gmail.com>
2024-11-02 23:05:56 -0400

    finish draft of weather-etl project

---
+++ weather-etl/README.md
@@ -0,0 +1,31 @@
+# Weather Bash Script Project
+
+## Data Source
+
+The data for this project is obtained from the open source project [wttr.in](https://wttr.in).  
+
+wttr.in is a web service that provides weather forecast information in a text-based format. 
+
+[wttr.in github](https://github.com/chubin/wttr.in#readme)
+
+## Functionality
+
+The project will do the following -
+
+Objective - a daily log in tabular form of the observed temperature and the forecasted temperature for the following day at noon.
+
+- Download raw weather data
+- Extract data of interest from the raw data
+- Transform the data as required
+- Load the data into a log file using a tabular format
+- Schedule the entire process to run automatically at a set time daily
+
+## Files
+
+weather.sh - the main script that generates a record of relevant data (one line of data)
+weather.log - the log file that contains the data for each day
+weather_report - a file that contains the daily weather report
+
+## variables
+current_temp - the current temp
+fc_temp - the temp forcasted for the following day
--- weather-etl/weather.sh
+++ weather-etl/weather.sh
@@ -5,29 +5,27 @@ city=Casablanca
 #-----------------
 # Step 1
 
-# uses curl to obtain weather data for Casablanca from the wttr api
-# and pipes the response into weather_report
+# use curl to obtain weather report data for Casablanca from the wttr api
+# and output the response to weather_report
 
 # ?T after the $city variable disable colors per the api docs
-# curl -s wttr.in/$city?T --output weather_report
-
+curl -s wttr.in/$city?T --output weather_report
 
 #-----------------
 # Step 2
 
-# extracts the current temp from the returned data and prints to the console
+# extract the current temp from the returned data
 
 current_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°' | grep -Eo -e '-?[[:digit:]].*')
-# echo "The current temp of $city is $current_temp"
-
+# echo "The current temp of $city is ${current_temp}"
 
 #-----------------
 # Step 3
 
-# extracts the forcasted temp for tomorrrow at noon and prints to the console
+# extract the forcasted temp for tomorrrow at noon
 
 fc_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | cut -d 'F' -f2 | grep -Eo -e '-?[[:digit:]].*')
-#echo "The temp forcasted for tomorrow in $city is $fc_temp F"
+# echo "The temp forcasted for tomorrow in $city is ${fc_temp}F"
 
 #-----------------
 # Step 4
@@ -45,7 +43,10 @@ year=$(TZ='Morocco/Casablanca' date +%Y)
 #-----------------
 # Step 5
 
-# merge fields into tab-delimited record corresponding to a single row
+# log the weather
 
-record=$(echo -e "$year\t$month\t$day\t$current_temp\t$fc_temp F")
+# merge fields into tab-delimited record corresponding to a single row &
+# append the data to weather.log
+
+record=$(echo -e "$year\t$month\t$day\t$current_temp\t${fc_temp}F")
 echo $record>>weather.log
